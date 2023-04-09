aae554a987a106e86d4b5b9c8fc6ec3bb629963b
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-09 11:23:20 +0100

    Add files via upload

--- test01_sum.cpp
+++ test01_sum.cpp
@@ -1,22 +1,44 @@
+#include <ios>       // std::boolalpha
 #include <iostream>  // std::cout/endl
 
 using namespace std;
 
-template <typename T>
-constexpr auto sum(T x)
+template <typename... Args>
+constexpr auto sum1l(Args... args)
 {
-    return x;
+    return (... + args);
 }
 
-template <typename T1, typename T2, typename... Targ>
-constexpr auto sum(T1 x, T2 y, Targ... args)
+template <typename... Args>
+constexpr auto sum1r(Args... args)
 {
-    return sum(x + y, args...);
+    return (args + ...);
+}
+
+template <typename... Args>
+constexpr auto sum2l(Args... args)
+{
+    return (0 + ... + args);
 }
 
 int main()
 {
-    short x = 12345;
-    auto result = sum(1, 2, 3.5, x);
-    cout << result << endl;
+    cout << boolalpha;
+
+    auto s1 = sum1l(1, 2, 3, 4, 5);
+    cout << "sum1l(1, 2, 3, 4, 5) = " << s1 << endl;
+
+    auto s2 = sum1l(1e16, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
+    cout << "sum1l(1e16, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2) == 1e16: "
+         << (s2 == 1e16) << endl;
+    auto s3 = sum1r(1e16, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2);
+    cout << "sum1r(1e16, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2) == 1e16: "
+         << (s3 == 1e16) << endl;
+
+    auto s4 = sum2l(1, 2, 3, 4, 5);
+    cout << "sum2l(1, 2, 3, 4, 5) = " << s4 << endl;
+
+    // auto s5 = sum1l(); // won't compile
+    auto s6 = sum2l();
+    cout << "sum2l() = " << s6 << endl;
 }
